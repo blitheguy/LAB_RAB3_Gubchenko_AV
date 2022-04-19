a8b0a9f54f8dffd73d9d185d1d0fa3a5574c6456
blitheguy <103589017+blitheguy@users.noreply.github.com>
2022-04-19 18:26:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5};
+        int sum = 0;
+        for (int i=0; i<4; i++){
+            sum+=arr[i];
+        }
+    System.out.println("Cумма чисел массива = " + sum);
+    }
+}
